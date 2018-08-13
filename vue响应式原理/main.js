f12b2db9d467548230c957510295d4f3db220312
lilingxia <lilx@sz-bcs.com.cn>
2018-08-13 18:11:32 +0800

    添加我的笔记备注

--- vue响应式原理/main.js
+++ vue响应式原理/main.js
@@ -1,11 +1,13 @@
 //watcher
 //selfValue exp cb
 function Watcher(vm, exp, cb) {
-    //用作构造函数    这里的this是指Watcher的实例
+    //用作构造函数     这里的this是指Watcher的实例
     this.cb = cb;
     this.vm = vm;
     this.exp = exp;
     this.value = this.get();  // 将自己添加到订阅器的操作
+
+    //本watcher的value被赋值为  vm.value
 }
  
 Watcher.prototype = {
@@ -13,16 +15,20 @@ Watcher.prototype = {
         this.run();
     },
     run: function() {
+        //获取现在的值
         var value = this.vm.data[this.exp];
+        //获取以前的值
         var oldVal = this.value;
-        if (value !== oldVal) {
+        if (value !== oldVal) {//如果不相等
+            //更新这个watcher的值 以value oldVal 做参数执行函数cb
             this.value = value;
             this.cb.call(this.vm, value, oldVal);
         }
     },
-    get: function() {
+    get: function() {//这个get只在构造函数使用
+        //这个Dep是做什么的 是不是全局的变量
         Dep.target = this;  // 缓存自己
-        var value = this.vm.data[this.exp]  // 强制执行监听器里的get函数
+        var value = this.vm.data[this.exp]  // 强制执行监听器里的get函数  获取exp的值
         Dep.target = null;  // 释放自己
         return value;
     }
@@ -37,25 +43,33 @@ Dep.prototype = {
     },
     notify: function() {
         this.subs.forEach(function(sub) {
-            sub.update();
+            sub.update(); //执行update观察者的函数
         });
     }
 };
 
 //observer
 function defineReactive(data, key, val) {
+
     observe(val); // 递归遍历所有子属性
-    var dep = new Dep(); 
+
+    //先不看递归分析 此函数
+    //data 定义一个新属性 key 值为val
+    var dep = new Dep(); //函数里面的局部变量
     Object.defineProperty(data, key, {
         enumerable: true,
         configurable: true,
         get: function() {
+            //每次获取这个值的时候，判断是否需要添加订阅者  (可是target为什么是一个全局的呢，这样如何绑定属性和订阅者的关系)
+            //因为每次绑定完会立刻执行get函数，划重点 js是单线程，可以这么干
             if (Dep.target) { // 判断是否需要添加订阅者
                 dep.addSub(Dep.target); // 在这里添加一个订阅者
             }
             return val;
         },
         set: function(newVal) {
+            //每次设置这个值的时候,
+            //通知
             if (val === newVal) {
                 return;
             }
