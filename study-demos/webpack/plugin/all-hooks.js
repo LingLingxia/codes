ca149f9c5532e09e349a02b9b9435f6d0e7f1e7e
lilx <lilx@authine.com>
2019-01-16 19:47:07 +0800

    上传新笔记

--- study-demos/webpack/plugin/all-hooks.js
+++ study-demos/webpack/plugin/all-hooks.js
@@ -47,48 +47,53 @@ compiler.hooks.afterEnvironment.tap('afterEnvironment',function(){
  });
 
   compiler.hooks.normalModuleFactory.tap('normalModuleFactory',function(normalModuleFactory){
+    // normalModuleFactory 8
     debugger;
 
 });
 
 compiler.hooks.contextModuleFactory.tap('contextModuleFactory',function(contextModuleFactory){
+  // normalModuleFactory 9
   debugger;
  
 });
 
 compiler.hooks.beforeCompile.tap('beforeCompile',function(compilationParams){
   debugger;
-  // compilationParams 由一些自有依赖和上面的 normalModuleFactory ,contextModuleFactory构成。
-   //编译参数创建之后执行。
+  // compilationParams 由一些自有依赖和上面的 normalModuleFactory ,contextModuleFactory构成。 10
+  //编译参数创建之后执行。
 });
 
 compiler.hooks.compile.tap('compile',function(compilationParams){
   debugger;
-   //一个新的编译创建之后执行。在有devserver的情况下应该会起作用。
+   //一个新的编译创建之后执行。在有devserver的情况下应该会起作用。 11
+   //compilation的钩子在这里之后将不再生效
 });
 
 
 compiler.hooks.thisCompilation.tap('thisCompilation',function(compilation){
   debugger;
-   //一个新的编译创建之后执行。在有devserver的情况下应该会起作用。
+   //一个新的编译创建之后执行。在有devserver的情况下应该会起作用。 12
 });
 
-compiler.hooks.compilation.tap('compilation',function(compilation){
+compiler.hooks.compilation.tap('compilation',function(compilation){ 
   debugger;
-   //编译创建之后执行。需要再devserver的情况下验证和compile的关系
+   //编译创建之后执行。需要再devserver的情况下验证和compile的关系 13
 });
 
 compiler.hooks.make.tap('make',function(compilation){
-  //无参数  ，异步平行钩子?
+  //无参数  ，异步平行钩子?  14
   debugger;
 });
 
 compiler.hooks.afterCompile.tap('afterCompile',function(compilation){
+    // 15
   debugger;
 });
 
 
 compiler.hooks.shouldEmit.tap('shouldEmit',function(compilation){
+  //16
   debugger;
 });
 
@@ -98,16 +103,17 @@ compiler.hooks.shouldEmit.tap('shouldEmit',function(compilation){
 // });
 
 compiler.hooks.emit.tap('emit',function(compilation){
-  //生成资源到 output 目录之前。
+
+  //生成资源到 output 目录之前。 17
   debugger;
 });
 compiler.hooks.afterEmit.tap('afterEmit',function(compilation){
-  //生成资源到 output 目录之后。
+  //生成资源到 output 目录之后。  18
   debugger;
 });
 
 compiler.hooks.done.tap('done',function(stats){
-  //编译完成
+  //编译完成  19
   debugger;
 });
 
