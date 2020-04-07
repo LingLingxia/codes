a8a9d83de3a1f7b6fc3855ffa8b302f5963e5b9e
LingLingxia <llxlansing@qq.com>
2020-04-07 21:46:03 +0800

    refactor:压缩图片文件，减少体积，删除无用代码

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 记录我的学习进程
+  - day-by-day/leetCode
 ## 各技术栈学习笔记
+ - study-demos
 ## 可复用代码整理
+- day-by-day/some-util-js
+- 
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git status
-echo "input commit message"
-read message
-git add .
-git commit -m message
-git push
