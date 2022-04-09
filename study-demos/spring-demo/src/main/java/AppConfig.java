1487eab1a0d6ca1ed39122b11fe2484215db1c0c
lansing <llxlansing@qq.com>
2022-04-09 21:48:20 +0800

    add demo1 for spring

---
+++ study-demos/spring-demo/src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpel;
+import com.pluralsight.repository.SpeakerRepository;
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService(){
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+      //  service.setRepository(getSpeakerRepository());
+        return service;
+        //return new SpeakerServiceImpl();
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository(){
+        return  new HibernateSpeakerRepositoryImpel();
+    }
+}
---
+++ study-demos/spring-demo/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpel;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    //private SpeakerRepository repository = new HibernateSpeakerRepositoryImpel();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Speaker> findAll(){
+       return repository.findAll();
+    }
+
+//    public void setRepository(SpeakerRepository repository) {
+//        this.repository = repository;
+//    }
+}
