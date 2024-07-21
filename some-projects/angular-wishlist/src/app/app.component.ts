1198e94acce88a4286a1333cbee6feca4f283e82
LingLingxia <llxlansing@qq.com>
2024-07-21 16:58:37 -0700

    add filter for wishlist

--- some-projects/angular-wishlist/src/app/app.component.ts
+++ some-projects/angular-wishlist/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
+
+const filters = [
+  (item:WishItem)=>item,
+  (item:WishItem)=>!item.isComplete,
+  (item:WishItem)=>item.isComplete,
+]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +18,15 @@ export class AppComponent {
     new WishItem("Find grass that cuts itself")
   ];
   title = 'my wishlist';
+  newWishText = "";
+  listFilter: String = "0";
+  get visibleItems() :WishItem[] {
+     return this.items.filter(filters[Number(this.listFilter)])
+  }
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = "";
+  }
   toggleItem(item:WishItem){
     item.isComplete = !item.isComplete;
     console.log(item);
