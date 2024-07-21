1a545bc720d797da73c805c0561455353ab91aa3
LingLingxia <llxlansing@qq.com>
2024-07-21 12:29:10 -0700

    archive document

--- day-by-day/leetCode-java/src/FanXingFunction.java
+++
@@ -1,9 +0,0 @@
-public class FanXingFunction{
-    public static <T> T getMid(T...a){
-        return a[a.length/2];
-    }
-
-    public static void main(String[] args) {
-       int c =  FanXingFunction.getMid(1,2,3,4);
-    }
-}
--- day-by-day/leetCode-java/src/Lambda.java
+++
@@ -1,5 +0,0 @@
-public class Lambda {
-    public static void main(String[] args) {
-
-    }
-}
